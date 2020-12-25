33a6b9fbf7e80e79c9cabec7a4501bdd216273c8
miureus <miureus@gmail.com>
2020-12-25 12:49:46 +0700

    update stopwatch

--- src/screens/RunScreen.js
+++ src/screens/RunScreen.js
@@ -1,66 +1,128 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 import * as Animatable from 'react-native-animatable';
 import Ionicons from 'react-native-vector-icons/Ionicons'
-import Entypo from 'react-native-vector-icons/Entypo'
 
-const RunScreen = ({navigation}) => {
-    return (
-        <>
-            <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
-            <SafeAreaView style={styles.container}>
-                <View style={styles.ladding}>
-                    <View style={styles.headerContainer}>
+export default class RunScreen extends Component {
+    constructor(props) {
+        super(props);
+     
+        this.state = {
+          timer: null,
+          minutes_Counter: '00',
+          seconds_Counter: '00',
+          startDisable: false,
+        }
+      }
+     
+      componentWillUnmount() {
+        clearInterval(this.state.timer);
+      }
+     
+      onButtonStart = () => {
+     
+        let timer = setInterval(() => {
+     
+          var num = (Number(this.state.seconds_Counter) + 1).toString(),
+            count = this.state.minutes_Counter;
+     
+          if (Number(this.state.seconds_Counter) == 59) {
+            count = (Number(this.state.minutes_Counter) + 1).toString();
+            num = '00';
+          }
+     
+          this.setState({
+            minutes_Counter: count.length == 1 ? '0' + count : count,
+            seconds_Counter: num.length == 1 ? '0' + num : num
+          });
+        }, 1000);
+        this.setState({ timer });
+     
+        this.setState({startDisable : true})
+      }
+     
+     
+      onButtonStop = () => {
+        clearInterval(this.state.timer);
+        this.setState({startDisable : false})
+      }
+     
+     
+      onButtonClear = () => {
+        clearInterval(this.state.timer);
+        this.setState({
+          timer: null,
+          minutes_Counter: '00',
+          seconds_Counter: '00',
+          startDisable : false,
+        });
+        this.props.navigation.navigate('EndActivity')
+    }
 
-                    </View>
-                    <Animatable.View 
-                        style={styles.footer}
-                        animation="fadeInUpBig"
-                        >
-                            <ScrollView
-                            showsVerticalScrollIndicator={false}
-                            showsHorizontalScrollIndicator={false}  
+    render() {
+        return (
+            <>
+                <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
+                <SafeAreaView style={styles.container}>
+                    <View style={styles.ladding}>
+                        <View style={styles.headerContainer}>
+
+                        </View>
+                        <Animatable.View 
+                            style={styles.footer}
+                            animation="fadeInUpBig"
                             >
-                                <View style={styles.buttonRun}>
-                                    <TouchableOpacity style={styles.iconOval} activeOpacity={0.5} onPress={() => navigation.goBack()}>
-                                        <Ionicons name='md-chevron-back' color='#000' size={30} />
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={styles.iconGo} activeOpacity={0.5} onPress={() => navigation.navigate('EndActivity')}>
-                                        <Text style={{fontSize: 22, color: '#000', fontWeight: '700'}}>GO</Text>
-                                    </TouchableOpacity>  
-                                    <TouchableOpacity style={styles.iconOval} activeOpacity={0.5} >
-                                        <Entypo name='dots-two-vertical' color='#000' size={30}  />
-                                    </TouchableOpacity>    
-                                </View>
-                                <View style={styles.line}></View>
-                                <View style={styles.distance}>
-                                    <Text style={{color: '#000', fontWeight: '700', fontSize: 30}}>00:00:00</Text>
-                                    <Text style={{color: '#000', fontWeight: '700', fontSize: 12}}>TIME</Text>
-                                </View>
-                                <View style={styles.infoRunning}>
-                                    <View style={{flexDirection: 'column', alignItems: 'center'}}>
-                                        <Text style={{color: '#000', fontSize: 15}}>Distance</Text>
-                                        <Text style={styles.textInfo}>0,0 <Text style={{fontSize: 10, fontWeight: '700'}}>KM</Text></Text>
+                                <ScrollView
+                                showsVerticalScrollIndicator={false}
+                                showsHorizontalScrollIndicator={false}  
+                                >
+                                    <View style={styles.buttonRun}>
+                                        <TouchableOpacity style={styles.iconOval} activeOpacity={0.5} onPress={() => this.props.navigation.goBack()}>
+                                            <Ionicons name='md-chevron-back' color='#000' size={30} />
+                                        </TouchableOpacity>
+                                        <TouchableOpacity 
+                                        activeOpacity={0.5} 
+                                        onPress={this.onButtonStart}
+                                        style={[styles.iconGo, { backgroundColor:  this.state.startDisable ? '#e48c12' : '#fff'}]} 
+                                        disabled={this.state.startDisable}>
+                                            <Text style={{fontSize: 22, color: '#000', fontWeight: '700'}}>GO</Text>
+                                        </TouchableOpacity>  
+                                        <TouchableOpacity 
+                                        activeOpacity={0.5} 
+                                        onPress={this.onButtonClear}
+                                        style={[styles.iconOval2, { backgroundColor:  this.state.startDisable ? '#000' : '#e48c12'}]} 
+                                        >
+                                            <Ionicons name='stop' size={30}  color={this.state.startDisable? '#fff' : '#000'}/>
+                                        </TouchableOpacity>    
                                     </View>
-                                    <View style={{flexDirection: 'column', alignItems: 'center'}}>
-                                        <Text style={{color: '#000', fontSize: 15}}>Pace</Text>
-                                        <Text style={styles.textInfo}>--</Text>
+                                    <View style={styles.line}></View>
+                                    <View style={styles.distance}>
+                                        <Text style={styles.counterText}>{this.state.minutes_Counter} : {this.state.seconds_Counter}</Text>
+                                        <Text style={{color: '#000', fontWeight: '700', fontSize: 12}}>TIME</Text>
                                     </View>
-                                    <View style={{flexDirection: 'column', alignItems: 'center'}}>
-                                        <Text style={{color: '#000', fontSize: 15}}>Calories</Text>
-                                        <Text style={styles.textInfo}>0 <Text style={{fontSize: 10, fontWeight: '700'}}>KCAL</Text></Text>
+                                    <View style={styles.infoRunning}>
+                                        <View style={{flexDirection: 'column', alignItems: 'center'}}>
+                                            <Text style={{color: '#000', fontSize: 15}}>Distance</Text>
+                                            <Text style={styles.textInfo}>0,0 <Text style={{fontSize: 10, fontWeight: '700'}}>KM</Text></Text>
+                                        </View>
+                                        <View style={{flexDirection: 'column', alignItems: 'center'}}>
+                                            <Text style={{color: '#000', fontSize: 15}}>Pace</Text>
+                                            <Text style={styles.textInfo}>--</Text>
+                                        </View>
+                                        <View style={{flexDirection: 'column', alignItems: 'center'}}>
+                                            <Text style={{color: '#000', fontSize: 15}}>Calories</Text>
+                                            <Text style={styles.textInfo}>0 <Text style={{fontSize: 10, fontWeight: '700'}}>KCAL</Text></Text>
+                                        </View>
                                     </View>
-                                </View>
-                            </ScrollView>
-                    </Animatable.View>
-                </View>
-            </SafeAreaView>
-        </>
-    )
+                                </ScrollView>
+                        </Animatable.View>
+                    </View>
+                </SafeAreaView>
+            </>
+        )
+    }
 }
-
-export default RunScreen
 
 const styles = StyleSheet.create({
     container: {
@@ -93,12 +155,17 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingHorizontal: 25,
+        paddingHorizontal: 20,
         marginTop: 20,
     },
     iconOval: {
         backgroundColor: '#e48c12',
-        padding: 8,
+        padding: 12,
+        borderRadius: 120,
+    },
+    iconOval2: {
+        backgroundColor: '#000',
+        padding: 12,
         borderRadius: 120,
     },
     iconGo: {
@@ -134,6 +201,11 @@ const styles = StyleSheet.create({
         color: '#000', 
         fontWeight: '700', 
         fontSize: 22
+    },
+    counterText: {
+        color: '#000', 
+        fontWeight: '700', 
+        fontSize: 30
     }
 })
 
